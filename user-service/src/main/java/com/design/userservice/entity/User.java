23ef263ca8e866ba5395ccb9bf1269a443078072
romanpryimuk <prijmuk1711@gmail.com>
2023-04-11 00:14:38 +0300

    Added swagger

--- user-service/src/main/java/com/design/userservice/entity/User.java
+++ user-service/src/main/java/com/design/userservice/entity/User.java
@@ -1,11 +1,12 @@
 package com.design.userservice.entity;
 
-import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
 import java.util.List;
 
 
@@ -18,5 +19,6 @@ public class User extends AbstractBaseEntity {
 
     private String name;
 
+    @ElementCollection
     private List<Long> productIds;
 }
--- user-service/src/main/java/com/design/userservice/rest/UserController.java
+++ user-service/src/main/java/com/design/userservice/rest/UserController.java
@@ -3,13 +3,15 @@ package com.design.userservice.rest;
 import com.design.dtoservice.user_service.UserDto;
 import com.design.userservice.entity.User;
 import com.design.userservice.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
-@RestController("/users")
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
@@ -19,13 +21,15 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<UserDto> getUser(@PathVariable Long id) {
-        UserDto foundUser = userService.findById(id);
+    public ResponseEntity<UserDto> getUser( @PathVariable String  id) {
+
+        UserDto foundUser = userService.findById(Long.parseLong(id));
         log.info("Found user {}", foundUser);
         return new ResponseEntity<>(foundUser, HttpStatus.OK);
     }
 
     @PostMapping("/")
+    @Operation(summary = "Get users", description = "Get list of users")
     public ResponseEntity<UserDto> postUser(@RequestBody User user) {
         UserDto savedUser = userService.save(user);
         log.info("Saved user {}", savedUser);
