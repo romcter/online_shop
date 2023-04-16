e663f4cd9082b04468d15da7d50c1e1a9326e0e1
romcter <66554495+romcter@users.noreply.github.com>
2023-04-16 18:58:40 +0300

    Feature/add feign client to user service (#4)
    * added feign client and functionality for create user assign created bucket to him
    
    * delete useless functionality from scheduler

--- user-service/src/main/java/com/design/userservice/rest/feignClient/OrderClient.java
+++ user-service/src/main/java/com/design/userservice/rest/feignClient/OrderClient.java
@@ -1,18 +1,13 @@
-//package com.design.userservice.rest.feignClient;
-//
-//import org.springframework.cloud.openfeign.FeignClient;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//
-//import java.util.List;
-//
-//@FeignClient(value = "jplaceholder", url = "http://localhost:8081/orders")
-//public interface OrderClient {
-//
-//        @RequestMapping(method = RequestMethod.GET, value = "/")
-//        List<OrderDto> getOrders();
-//
-//        @RequestMapping(method = RequestMethod.GET, value = "/{orderId}/", produces = "application/json")
-//        OrderDto getOrderById(@PathVariable("orderId") Long orderId);
-//}
+package com.design.userservice.rest.feignClient;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(value = "user-service", url = "http://localhost:8081/bucket")
+public interface OrderClient {
+
+    @RequestMapping(method = RequestMethod.POST, value = "/product")
+    Long createAndAssignBucketToUser(@RequestBody Long userId);
+}
