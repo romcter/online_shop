b02b35dd462600aac984e90eddd8530840310aa4
romanpryimuk <prijmuk1711@gmail.com>
2023-04-21 17:41:38 +0300

    containerize product-service, add plugin to pom in user-service

---
+++ product-service/src/main/java/com/design/productservice/config/EndpointsListener.java
@@ -0,0 +1,18 @@
+package com.design.productservice.config;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+@Component
+public class EndpointsListener implements ApplicationListener<ContextRefreshedEvent> {
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        ApplicationContext applicationContext = event.getApplicationContext();
+        applicationContext.getBean(RequestMappingHandlerMapping.class).getHandlerMethods()
+                .forEach((key, value) -> System.out.println( "=====================" + key + "================" + value));
+    }
+}
